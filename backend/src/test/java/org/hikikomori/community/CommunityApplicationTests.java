99949cf17dbb5d62f545a92e2accdc0e45b41c6c
dja1369 <qjxlftn@gmail.com>
2026-02-02 21:39:11 +0900

    init

---
+++ backend/src/test/java/org/hikikomori/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hikikomori.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
