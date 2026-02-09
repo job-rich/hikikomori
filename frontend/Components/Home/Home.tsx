adcc9a04c7ffd95167228d377a5e0bc0bfa8d9e0
HyeontaeKim0 <hyeontae.kim@wincl.io>
2026-02-09 17:49:12 +0900

    delete : 테스트 overlay 제거

--- frontend/Components/Home/Home.tsx
+++ frontend/Components/Home/Home.tsx
@@ -3,15 +3,10 @@
 import Navbar from '@/Components/Home/Navbar/Navbar';
 import Body from '@/Components/Home/Body/Body';
 import Footer from '@/Components/Home/Footer/Footer';
-import { RealityGlitch } from '../Common/Overlay/RealityGlitch';
-import { FloatingEntity } from '../Common/Overlay/FloatingEntity';
-import { ScanlineOverlay } from '../Common/Overlay/ScanLine';
+
 export default function Home() {
   return (
     <>
-      <ScanlineOverlay />
-      <RealityGlitch />
-      <FloatingEntity />
       <Navbar />
       <Body />
       <Footer />
