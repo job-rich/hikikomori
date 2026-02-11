84816df8a6f1e1ec3de66a73cbf0281ddae0c4e7
sshworld <59462537+sshworld@users.noreply.github.com>
2026-02-11 23:53:05 +0900

    style: code style 적용

--- backend/src/main/java/org/hikikomori/community/domain/Comment.java
+++ backend/src/main/java/org/hikikomori/community/domain/Comment.java
@@ -1,6 +1,12 @@
 package org.hikikomori.community.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
