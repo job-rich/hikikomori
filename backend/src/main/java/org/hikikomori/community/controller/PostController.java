5e9eb05a9449825aea63e40f274b45334c7b3ac2
SSH._.WORLD <ssh97540@gmail.com>
2026-03-22 21:20:42 +0900

    Feature/post comment update delete (#23)
    * feat: 게시글, 댓글 수정 삭제 기능 추가
    
    * fix: comment response 수정
    
    * test: 게시글 댓글 수정 삭제 테스트 추가

--- backend/src/main/java/org/hikikomori/community/controller/PostController.java
+++ backend/src/main/java/org/hikikomori/community/controller/PostController.java
@@ -6,8 +6,10 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.hikikomori.community.controller.data.CommentCreateRequest;
 import org.hikikomori.community.controller.data.CommentResponse;
+import org.hikikomori.community.controller.data.CommentUpdateRequest;
 import org.hikikomori.community.controller.data.PostCreateRequest;
 import org.hikikomori.community.controller.data.PostResponse;
+import org.hikikomori.community.controller.data.PostUpdateRequest;
 import org.hikikomori.community.domain.Comment;
 import org.hikikomori.community.domain.Post;
 import org.hikikomori.community.service.PostService;
@@ -16,11 +18,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,6 +56,22 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(PostResponse.from(post));
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostResponse> update(
+            @PathVariable UUID id,
+            @Valid @RequestBody PostUpdateRequest request) {
+        postService.update(id, request);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id, @RequestParam Long userId) {
+        postService.delete(id, userId);
+
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping("/{id}/comments")
     public ResponseEntity<List<CommentResponse>> findComments(@PathVariable UUID id) {
         List<CommentResponse> comments = postService.findCommentsByPostId(id).stream()
@@ -68,5 +89,27 @@ public class PostController {
         Comment comment = postService.createComment(id, request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(CommentResponse.from(comment));
+    }
+
+    @PatchMapping("/{id}/comments/{commentId}")
+    public ResponseEntity<Void> updateComment(
+            @PathVariable UUID id,
+            @PathVariable UUID commentId,
+            @Valid @RequestBody CommentUpdateRequest request
+    ) {
+        postService.updateComment(commentId, request);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(
+            @PathVariable UUID id,
+            @PathVariable UUID commentId,
+            @RequestParam Long userId
+    ) {
+        postService.deleteComment(commentId, userId);
+
+        return ResponseEntity.noContent().build();
     }
 }
