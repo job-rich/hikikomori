be3d66ffa4370d8df4c163b6c95b74475b8206e3
sshworld <ssh97540@gmail.com>
2026-04-09 22:36:53 +0900

    style: 비교연산 스타일 수정

--- backend/src/main/java/org/hikikomori/community/dto/CommentDto.java
+++ backend/src/main/java/org/hikikomori/community/dto/CommentDto.java
@@ -40,7 +40,7 @@ public class CommentDto {
                     comment.getId(),
                     comment.getUserId(),
                     comment.getNickName(),
-                    comment.getDeletedAt() != null ? null : comment.getContent(),
+                    comment.getDeletedAt() == null ? comment.getContent() : null,
                     comment.getCreatedAt(),
                     comment.getUpdatedAt(),
                     comment.getDeletedAt(),
